90eca970e48a8c6936a4c8f8fbc1ed59591b91bf
BLooDBRothER <computertechnology446@gmail.com>
2021-12-13 20:25:54 +0530

    Add user login functions

---
+++ src/js/login_authentication.js
@@ -0,0 +1,33 @@
+import { onAuthStateChanged} from "firebase/auth";
+import { auth, login, logout } from "./modules/_login.js";
+import { user } from "./modules/_user.js";
+
+const loginBtn = document.querySelector(".login-btn");
+const userInfoSection = document.querySelector(".user-info");
+
+loginBtn.addEventListener("click", (e) => {
+  if(!user.getLoggedInStatus()){
+    login();
+  }
+  // user.getLoggedInStatus() ? logout() : login();
+});
+
+// Update User data
+function updateUserData(){
+  user.getLoggedInStatus() 
+   ? (userInfoSection.classList.remove("none"), loginBtn.classList.add("none"))
+   : (userInfoSection.classList.add("none"), loginBtn.classList.remove("none"));
+  userInfoSection.querySelector(".user-name").innerText = user.getName();
+  userInfoSection.querySelector(".user-profile-picture").src = user.getProfilPicUrl();
+}
+
+onAuthStateChanged(auth, (firebaseUser) => {
+  if(firebaseUser){
+    user.setInitial([firebaseUser.uid, firebaseUser.displayName, firebaseUser.photoURL]);
+    updateUserData();
+  }
+  else{
+    user.removeInitial();
+    updateUserData();
+  }
+});
