776e183edf345ee64137164bb46a03b3a9ec86ed
BLooDBRothER <computertechnology446@gmail.com>
2021-12-13 20:28:59 +0530

    Update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -45,5 +46,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "style.[hash].css"
     }),
+    new CleanWebpackPlugin()
   ],
 };
--- webpack.production.js
+++ webpack.production.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const common = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -10,5 +9,4 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "assets/[name]-[hash][ext][query]",
   },
-  plugins: [new CleanWebpackPlugin()],
 });
