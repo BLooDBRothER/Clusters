fb2ddca6314c50d5a3b2fffd821cdf220fb146d1
BLooDBRothER <computertechnology446@gmail.com>
2021-12-13 20:26:14 +0530

    update Imports

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// SASS
 import "../style-sheets/style.sass"
 
-console.clear()
-// Firebase Confit
-const firebaseConfig = {
-  apiKey: "AIzaSyDzPle3URDYs1O1M-jTMUy64ZzVfXWKb5g",
-  authDomain: "web-clusters.firebaseapp.com",
-  projectId: "web-clusters",
-  storageBucket: "web-clusters.appspot.com",
-  messagingSenderId: "757696261414",
-  appId: "1:757696261414:web:482a68317384eaee57a5e7",
-  measurementId: "G-6J5W1FPTTH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// JS
+import "./modules/firebase_credential"
+import "./modules/_login.js"
+import "./login_authentication.js"
