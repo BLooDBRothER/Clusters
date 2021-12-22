99b497734036a768136b55a7bd62ce5c7be55954
Maheshkumar <callmeelprofessor@gmail.com>
2021-12-22 09:14:40 +0530

    Add tabs

--- src/js/login_authentication.js
+++ src/js/login_authentication.js
@@ -1,32 +1,37 @@
-import { onAuthStateChanged} from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth, login, logout } from "./modules/_login.js";
 import { user } from "./modules/_user.js";
 
 const loginBtn = document.querySelector(".login-btn");
+const menuSection = document.querySelector(".menu");
 const userInfoSection = document.querySelector(".user-info");
 
 loginBtn.addEventListener("click", (e) => {
-  if(!user.getLoggedInStatus()){
+  if (!user.getLoggedInStatus()) {
     login();
   }
   // user.getLoggedInStatus() ? logout() : login();
 });
 
 // Update User data
-function updateUserData(){
-  user.getLoggedInStatus() 
-   ? (userInfoSection.classList.remove("none"), loginBtn.classList.add("none"))
-   : (userInfoSection.classList.add("none"), loginBtn.classList.remove("none"));
+function updateUserData() {
+  user.getLoggedInStatus()
+    ? (menuSection.classList.remove("none"), loginBtn.classList.add("none"))
+    : (menuSection.classList.add("none"), loginBtn.classList.remove("none"));
   userInfoSection.querySelector(".user-name").innerText = user.getName();
-  userInfoSection.querySelector(".user-profile-picture").src = user.getProfilPicUrl();
+  userInfoSection.querySelector(".user-profile-picture").src =
+    user.getProfilPicUrl();
 }
 
 onAuthStateChanged(auth, (firebaseUser) => {
-  if(firebaseUser){
-    user.setInitial([firebaseUser.uid, firebaseUser.displayName, firebaseUser.photoURL]);
+  if (firebaseUser) {
+    user.setInitial([
+      firebaseUser.uid,
+      firebaseUser.displayName,
+      firebaseUser.photoURL,
+    ]);
     updateUserData();
-  }
-  else{
+  } else {
     user.removeInitial();
     updateUserData();
   }
