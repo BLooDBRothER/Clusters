bc743497a472562772bd66f2623b2d69112d92a7
BLooDBRothER <computertechnology446@gmail.com>
2021-12-26 09:19:51 +0530

    Create update db method

--- src/js/modules/_database.js
+++ src/js/modules/_database.js
@@ -1,10 +1,14 @@
-import { getDatabase, set, ref, get, push } from "firebase/database";
+import { getDatabase, set, ref, get, push, update } from "firebase/database";
 import { app } from "./firebase_credential.js";
 
 const database = getDatabase(app);
 
 export function writeDB(reference, data){
     set(ref(database, reference), data)
+}
+
+export function updateDB(updateData){
+    update(ref(database), updateData);
 }
 
 export async function readDB(reference){
